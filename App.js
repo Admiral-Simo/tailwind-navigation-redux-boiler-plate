6540fa433fabfdbe15f4e0412e14cacec9d2dc83
Admiral-Simo <admiralsimo16@gmail.com>
2023-02-16 09:54:21 +0100

    fix bugs

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -3,8 +3,11 @@ import React from "react";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className='mt-20 px-3'>
+      <Text className='text-2xl text-slate-900'>Expo Boiler Plate</Text>
+      <Text className='text-slate-500'>No need to waste 30-60 minutes on setups anymore</Text>
+      <Text className='text-slate-500'>This setup inclues redux - navigation - tailwindcss</Text>
+      
     </View>
   );
 };
